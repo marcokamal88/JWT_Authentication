c4f5e8cecf3f2b5b1715bf3798a68b8f386fcc55
marcokamal88 <marcolkamal@gmail.com>
2024-04-07 17:44:20 +0200

    sending token in header

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import express, { json } from "express";
 import { hash, compare } from "bcrypt";
-import  jwt  from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 const app = express();
 app.use(json());
 const port = 3000;
@@ -21,28 +21,40 @@ app.post("/register", async (req, res) => {
   }
 });
 app.post("/login", async (req, res) => {
-  let myuser;
   let userFound = users.find((user) => user.username == req.body.username);
   console.log(userFound);
   if (!userFound) {
     res.send("Authentication failed :(");
+  } else {
+    const passwordMatch = await compare(req.body.password, userFound.password);
+    if (!passwordMatch) {
+      res.send("wrong password :(");
+    }
+    const token = jwt.sign({ username: userFound.username }, "marco", {
+      expiresIn: "1h",
+    });
+    res.send(token);
   }
-  else {const passwordMatch = await compare(
-    req.body.password,
-    userFound.password
-  );
-  if (!passwordMatch) {
-    res.send("wrong password :(");
+});
+
+app.get("/me", verifyToken, (req, res) => {
+  res.send(res.username);
+});
+function verifyToken(req, res, next) {
+  const bearerHeader = req.headers["authorization"];
+
+  if (bearerHeader) {
+    const bearer = bearerHeader.split(" ");
+    const bearerToken = bearer[1];
+    jwt.verify(bearerToken, "marco", (err, username) => {
+      if (err) return res.sendStatus(403);
+      res.username = username;
+      next();
+    });
+  } else {
+    res.sendStatus(403);
   }
-  
-  const token = jwt.sign({ }, "marco", {
-    expiresIn: "1h",
-  });
-  res.send(token);}
-});
-app.get("/users", (req, res) => {
-  res.send(users);
-});
+}
 app.listen(port, () => {
   console.log(`app listening on port ${port}`);
 });
